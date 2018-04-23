5ada901c5c985ee34ac177bc5c79c850765d4f18
Randall Carlisle <cisolutionsinc@gmail.com>
2018-04-23 14:04:26 -0700

    controller logic for calling getById() in model

--- src/controllers/snacks.js
+++ src/controllers/snacks.js
@@ -1,7 +1,9 @@
-const model = require('../model/snacks');
+const model = require('../models/snacks');
 
 getAll = (req, res, next) => {
+    console.log(req.query)
     const limit = req.query.limit;
+    console.log('limit: ', limit)
     const data = model.getAll(limit);
 
     if (!data) {
@@ -10,6 +12,24 @@ getAll = (req, res, next) => {
             message: `Could not list snacks of limit ${limit}`
         })
     }
-
     res.status(200).json({ data });
 }
+
+getById = (req, res, next) => {
+    const id = req.params.id
+    const snack = model.getById(id)
+  
+    if (!snack) {
+      return next({
+        status: 404,
+        message: `Could not find snack with id of ${id}`
+      })
+    }
+    res.status(200).json({ snack })
+  }
+
+module.exports = { 
+    getAll,
+    getById 
+}
+// create, update, deleteSnack }
