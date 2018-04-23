f81e78e1f301622c7977577f755979e822f827d1
Randall Carlisle <cisolutionsinc@gmail.com>
2018-04-23 15:38:58 -0700

    added route entries for update and delete functionality

--- src/routes/snacks.js
+++ src/routes/snacks.js
@@ -5,5 +5,7 @@ const controller = require('../controllers/snacks');
 router.get('/', controller.getAll);
 router.get('/:id', controller.getById);
 router.post('/', controller.create);
+router.put('/:id', controller.update)
+router.delete('/:id', controller.deleteSnack)
 
 module.exports = router;
