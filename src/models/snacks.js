44d65606796d883e2a8c00ad2324432cf798736b
Randall Carlisle <cisolutionsinc@gmail.com>
2018-04-23 14:56:27 -0700

    model logic to create new snack object and add to response returned

--- src/models/snacks.js
+++ src/models/snacks.js
@@ -2,18 +2,38 @@ const uuid = require('uuid/v4');
 const snacks = require('../../db/snacks');
 
 getAll = (limit) => {
-    console.log('limit: ', limit)
+    console.log('limit: ', limit);
     const listOfSnacks = (!limit) ? snacks : (limit > snacks.length) ? false : snacks.slice(0, limit);
     return listOfSnacks;
 }
 
 getById = (id) => {
-    const snack = snacks.find(snack => { return snack.id === id })
-    return snack
+    const snack = snacks.find(snack => { return snack.id === id });
+    return snack;
   }
+
+create = (body) => {
+    // console.log("BODY IN MODEL", body)
+    const errors = [];
+    let response;
+
+    if (!body.name) {
+        errors.push('Please fill in name field');
+        response = { errors }
+    }
+
+    const snack = {
+        id: uuid(),
+        name: body.name
+    }
+    snacks.push(snack);
+    response = snack;
+    return response;
+}  
 
 module.exports = { 
     getAll, 
-    getById
+    getById,
+    create
 }
-// create, update, deleteSnack }
+// update, deleteSnack }
