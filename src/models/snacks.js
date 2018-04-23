9fb12d0613a314e517e65f66ac778e6866156248
Randall Carlisle <cisolutionsinc@gmail.com>
2018-04-23 14:09:02 -0700

    model logic to find matching id in snacks collection

--- src/models/snacks.js
+++ src/models/snacks.js
@@ -1,6 +1,19 @@
-const snacks = requires(',,/../db/snacks');
+const uuid = require('uuid/v4');
+const snacks = require('../../db/snacks');
 
 getAll = (limit) => {
+    console.log('limit: ', limit)
     const listOfSnacks = (!limit) ? snacks : (limit > snacks.length) ? false : snacks.slice(0, limit);
     return listOfSnacks;
 }
+
+getById = (id) => {
+    const snack = snacks.find(snack => { return snack.id === id })
+    return snack
+  }
+
+module.exports = { 
+    getAll, 
+    getById
+}
+// create, update, deleteSnack }
